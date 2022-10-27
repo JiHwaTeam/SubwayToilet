101c9cdff514c13e69c42056a99e41856f82295f
YUHASUNG <yuha822@naver.com>
2022-10-27 23:47:30 +0900

    find, modify

--- FE/src/App.js
+++ FE/src/App.js
@@ -5,6 +5,8 @@ import PageNotice from "./pages/PageNotice";
 import PageSuggest from "./pages/PageSuggest";
 import MyPage from "./pages/MyPage";
 import Login from "./pages/Login";
+import Find from "./pages/Find";
+import Modify from "./Components/Modify";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/page-suggest" element={<PageSuggest />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/find" element={<Find />} />
+        <Route path="/modify" element={<Modify />} />
       </Routes>
     </Layout>
   );
